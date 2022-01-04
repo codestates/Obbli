beeeaf54c72e8445b3d69580db344db591fdaa9f
baek1008 <junu0810@hanmail.net>
2022-01-05 01:13:20 +0900

    User_CRUD

--- server/src/controllers/Person/Sign.ts
+++ server/src/controllers/Person/Sign.ts
@@ -1,29 +1,84 @@
-import repository from "typeorm";
-const Person = require("../../entity/Person");
+import { Person } from "../../entity/Person";
+const { makejwt, hash } = require("../function");
 
 module.exports = {
   Sign_in: {
     post: (req, res) => {
       //로그인
-      res.status(200).send("로그인하기");
+      const { user_id, pw_hash } = req.body;
+
+      if (!user_id || !pw_hash) {
+        res.status(400).json({ message: "Not a member." });
+      }
+
+      const member = Person.findOne({ user_id: user_id, pw_hash: pw_hash });
+      member.then((result) => {
+        if (!member) {
+          res.status(400).json({ message: "Not a member." });
+        }
+        console.log(result);
+        const { uuid, user_id, email } = result;
+        const access_Token = makejwt({ uuid, user_id, email }, "1h");
+        const refresh_Token = makejwt({ uuid, user_id, email }, "1d");
+
+        res
+          .cookie("refresh_token", `bearer ${refresh_Token}`, {
+            httpOnly: true,
+          })
+          .status(200)
+          .json({ access_token: access_Token });
+      });
     },
   },
-  Sign_up: {
-    post: (req, res) => {
-      //회원가입
-      const userId = req.body.ID;
-      const userPw = req.body.PW;
 
-      if (!userId || !userPw) {
+  Sign_up: {
+    post: async (req, res) => {
+      //회원가입
+      const { user_id, pw_hash, realname, professional, history, email } =
+        req.body;
+      //아이디 비밀번호 전달확인
+      if (!user_id || !pw_hash) {
         res.status(400).json({ message: "write ID,PW" });
-      } else {
-        //쿠키에 refreshToken과 헤더에 Token을 담기
-        const person = new Person();
-        person.user_id = userId;
-        person.pw_hash = userPw;
-        person.save();
-        res.status(200).json({ message: "welcome" });
       }
+
+      //아이디 중복여부 확인
+      const usercheck = await Person.findOne({ user_id: user_id });
+      console.log(usercheck);
+
+      if (usercheck) {
+        res.status(400).json({ message: "이미있는 아이디입니다." });
+      } else {
+        //쿠키에 refreshToken과 헤더에 Token을 담기(추후과제)
+        await Person.create({
+          user_id: user_id,
+          pw_hash: pw_hash,
+          realname: realname,
+          professional: professional,
+          history: history,
+          email: email,
+        }).save();
+
+        await Person.findOne({ user_id: user_id }).then((result) => {
+          const { uuid, user_id, email } = result;
+          const access_Token = makejwt({ uuid, user_id, email }, "1h");
+          const refresh_Token = makejwt({ uuid, user_id, email }, "1d");
+
+          res
+            .cookie("refresh_token", `bearer ${refresh_Token}`, {
+              httpOnly: true,
+            })
+            .status(200)
+            .json({ access_token: access_Token });
+        });
+      }
+    },
+  },
+  sign_out: {
+    post: (req, res) => {
+      res
+        .clearCookie("refresh_token")
+        .status(200)
+        .json({ message: "success log_out" });
     },
   },
 };
--- server/src/controllers/Person/UserInfo.ts
+++ server/src/controllers/Person/UserInfo.ts
@@ -1,16 +1,52 @@
+import { createQuery } from "mysql2/typings/mysql/lib/Connection";
+import { getConnection } from "typeorm";
+import { Person } from "../../entity/Person";
+const { solveToken } = require("../function");
+
 module.exports = {
   UserInfo: {
-    get: (req, res) => {
-      //유저 정보 가져오기
-      res.status(200).send("유저정보 가져오기");
+    get: async (req, res) => {
+      //유저 정보 가져오기/ 토큰해석 후 가져오는것으로 고려해볼것
+      const solveData = solveToken(req.headers.access_token);
+      const person_uuid: string = solveData.uuid;
+      await Person.findOne({ uuid: person_uuid }).then((result) => {
+        if (!result) {
+          res.status(400).json({ message: "이런사람 없습니다." });
+        } else {
+          console.log(result);
+          res.status(200).json({ UserInfo: result });
+        }
+      });
     },
-    patch: (req, res) => {
+    patch: async (req, res) => {
+      const solveData = solveToken(req.headers.access_token);
+      const person_uuid: string = solveData.uuid;
+      const { user_id, realname, professional, history, email, cellular } =
+        req.body;
+
+      await getConnection()
+        .createQueryBuilder()
+        .update(Person)
+        .set({
+          user_id: user_id,
+          realname: realname,
+          professional: professional,
+          history: history,
+          email: email,
+          cellular: cellular,
+        })
+        .where("uuid = :uuid", { uuid: person_uuid })
+        .execute();
+
       //유저정보 수정하기
       res.status(200).send("유저정보 수정하기");
     },
-    delete: (req, res) => {
+    delete: async (req, res) => {
       //유저정보 삭제하기
-      res.status(200).send("유저정보삭제하기");
+      const person_uuid = req.params.person_uuid;
+      await Person.delete({ uuid: person_uuid });
+
+      res.clearCookie("refresh_token").status(200).send("유저정보삭제하기");
     },
   },
 };
