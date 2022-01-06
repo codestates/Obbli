2684c87b1b9d57d0c60d727660e11dcd95c60212
junu0810 <junu0810@hanmail.net>
2022-01-06 11:34:15 +0900

    Update index.ts

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,11 +1,3 @@
-
-module.exports = {
-  Person: require("./Person"),
-  Org: require("./Org"),
-  Advert: require("./Advert"),
-  Application: require("./Applicaton"),
-};
-
 import Person from "./Person";
 import Org  from "./Org";
 import Application from "./Applicaton";
