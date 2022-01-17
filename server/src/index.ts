d86d3e9cc866c855df44f1dedefabd81f4971fff
baek1008 <junu0810@hanmail.net>
2022-01-17 17:25:59 +0900

    midpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,26 @@
+import cors from 'cors';
 import express from "express";
 import indexRouter from './router'
 import morgan from 'morgan'
 import "reflect-metadata";
 import { createConnection, getConnectionOptions } from "typeorm";
 import {Advert,Application,Org,Org_review,Person,Person_review,Position,Skill } from "./entity";
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(cors({
+  allowedHeaders: ['Authorization', 'Content-Type'],
+  origin: `http://${process.env.APP_DOMAIN}:${port}`,
+  credentials: true,
+  preflightContinue: false,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+}));
 
 app.use("/person", indexRouter.Person);
 app.use("/org", indexRouter.Org);
@@ -22,6 +31,7 @@ app.use("/application", indexRouter.Application);
 export default app;
 
 if (!module.parent) {
+  app.use(morgan("dev"));
   getConnectionOptions().then((config) => {
     Object.assign(config, {
       entities: [Skill,Person,Org,Advert,Position,Application,Org_review,Person_review,],
